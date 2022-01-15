e7f6bc4a1e919565730ce69e22f8de1271de1049
SvetlanaVaskevich <81557049+SvetlanaVaskevich@users.noreply.github.com>
2022-01-15 18:28:39 +0500

    Drag&Drop Test

---
+++ src/test/java/github/DragDropTest.java
@@ -0,0 +1,16 @@
+package github;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class DragDropTest {
+
+    @Test
+    void movingRectangleTest(){
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDropTo("#column-b");
+        $("#column-a").shouldHave(Condition.text("B"));
+    }
+}
